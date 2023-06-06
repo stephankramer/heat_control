ed1206c6a7627401a6946a9e55974b02ffddc4be
Stephan Kramer <s.kramer@imperial.ac.uk>
2023-06-06 16:50:36 +0100

    Heat control problem with adaptive gradient descent
    Test case from http://doi.org/10.1137/070683416

---
+++ gradient_descent.py
@@ -0,0 +1,114 @@
+from firedrake import *
+from pyroteus import recover_hessian, hessian_metric
+from firedrake.meshadapt import adapt, RiemannianMetric
+from firedrake_adjoint import *
+import numpy as np
+import time
+
+
+def interpolate_between(source, target):
+    if source.function_space() == target.function_space():
+        target.assign(source)
+        return
+    from firedrake.mg.utils import physical_node_locations
+    target_fs = target.function_space()
+    target_locations = physical_node_locations(target_fs).dat.data[:]
+    vom = VertexOnlyMesh(source.function_space().mesh(), target_locations, redundant=False)
+    if isinstance(target_fs.ufl_element(), VectorElement):
+        vom_space = VectorFunctionSpace(vom, "DG", 0)
+    else:
+        vom_space = FunctionSpace(vom, "DG", 0)
+    target_vom_f = interpolate(source, vom_space)
+    target.dat.data[:] = target_vom_f.dat.data
+
+
+def interpolate_to_new_function_space(source, V):
+    u = Function(V, name=source.name())
+    interpolate_between(source, u)
+    return u
+
+
+def adapt_mesh(mesh, u, q, target_complexity):
+    H = recover_hessian(u, method='L2')
+    metric = RiemannianMetric(hessian_metric(H))
+    metric.rename("metric")
+    import numpy as np
+    metric.set_parameters(
+            {'dm_plex_metric_target_complexity': target_complexity,
+             'dm_plex_metric_p': np.inf,
+             #  'dm_plex_metric_verbosity': 10,
+             'dm_plex_gradation_factor': 1.5,
+             'dm_plex_metric_a_max': 10,
+             'dm_plex_metric_h_min': 1e-5,
+             'dm_plex_metric_h_max': .1})
+    H = recover_hessian(u, method='L2')
+    metric2 = RiemannianMetric(hessian_metric(H))
+    metric.intersect(metric2)
+    metric.normalise()
+    mesh = adapt(mesh, metric)
+    return mesh
+
+
+class DefaultParams:
+    max_adapts = 100
+    iterations_per_adapt = 5
+    target_complexity = 200
+    max_complexity = 20000
+    initial_learning_rate = 1e-8
+    target_increase_factor = 1.5
+
+
+def minimize_gs(run_model, mesh0, ele, params, bounds=None):
+
+    p = DefaultParams()
+    for key, value in params.items():
+        setattr(p, key, value)
+
+    mesh = mesh0
+    logf = open('log.txt', 'w')
+    for i in range(p.max_adapts):
+        V = FunctionSpace(mesh, ele)
+        if i > 0:
+            u = interpolate_to_new_function_space(u, V)
+            q = interpolate_to_new_function_space(q, V)
+            gold = interpolate_to_new_function_space(gold, V)
+            qold = interpolate_to_new_function_space(qold, V)
+        else:
+            u = Function(V)
+            q = Function(V)
+            gold = Function(V)
+            qold = Function(V)
+        g = Function(V)
+        for j in range(p.iterations_per_adapt):
+            tape = get_working_tape()
+            tape.clear_tape()
+            c = Control(q)
+            J, u = run_model(q, initial_guess=u)
+            rf = ReducedFunctional(J, c)
+            print(f"i, J = {i, J}")
+            g = rf.derivative(options={"riesz_representation": "L2"})
+            if i+j == 0:
+                alpha = p.initial_learning_rate
+            else:
+                dq = q - qold
+                dg = g - gold
+                alpha = np.abs(assemble(dot(dq, dq)*dx)/assemble(dot(dq, dg)*dx))
+            qold.assign(q)
+            gold.assign(g)
+            gnorm = np.sqrt(assemble(dot(g, g)*dx))
+            print(f"|g| = {gnorm}, alpha = {alpha}")
+            logf.write(f"{i}, {time.process_time()}, {J}, {gnorm}, {alpha}, {len(g.dat.data)}\n")
+            logf.flush()
+            q.assign(qold - alpha*g)
+            if bounds:
+                q.interpolate(max_value(bounds[0], min_value(bounds[1], q)))
+
+        with stop_annotating():
+            print("ADAPTING THE MESH:")
+            mesh = adapt_mesh(mesh, u, q, p.target_complexity)
+            p.target_complexity *= p.target_increase_factor
+            if p.target_complexity > p.max_complexity:
+                print("Maximum complexity reached")
+                break
+
+    return q
---
+++ vexler.py
@@ -0,0 +1,70 @@
+from firedrake import *
+from firedrake_adjoint import *
+from gradient_descent import minimize_gs
+
+mesh = Mesh('square_with_hole.msh')
+
+# we actually want to optimise for q
+# but we use q=beta*qc and use qc as the control
+# this way we can rescale the gradient dJ/dqc=dJ/dq dq/dqc=beta dJ/dq
+# we choose beta such dJ/dq is of order 1 in the initial optimisation iterations
+# this is because L-BFGS-B behaves poorly with gradients that are too large or too small
+# in the initial iterations when it doesn't have a very accurate Hessian approximation yet
+beta = 1e+6
+
+outf = File('qopt.pvd', adaptive=True)
+
+
+def run_model(control, initial_guess=None):
+    V = control.function_space()
+    mesh = V.mesh()
+
+    u = initial_guess or Function(V, name="Solution")
+    v = TestFunction(V)
+    q = Function(V, name='Control')
+
+    x1, x2 = SpatialCoordinate(mesh)
+    f = 1/((x1-0.5)**2 + (x2-0.5)**2)
+
+    F = (
+            dot(grad(u), grad(v)) * dx
+            + 30 * u**3 * v * dx
+            + u * v * dx
+            - (f + q)*v*dx
+            )
+    bc = DirichletBC(V, 0, [1, 2])
+
+    q.assign(beta*control)
+
+    solve(F == 0, u, bcs=bc, solver_parameters={'snes_converged_reason': None})
+
+    x1, x2 = SpatialCoordinate(mesh)
+    ud = x1*x2
+    udf = Function(V, name='Desired Solution').interpolate(ud)
+    alpha = 1e-4
+    J = assemble(0.5 * dot(u-ud, u-ud) * dx + alpha/2 * dot(q, q) * dx)
+
+    outf.write(q, u, udf)
+    return J, u
+
+
+ele = FiniteElement("CG", triangle, 1)
+# bounds for q are -7<q<20
+# but the optimisation algorithm deals with qc=q/beta
+bounds = [-7/beta, 20/beta]
+
+if False:
+    # fixed mesh with L-BFGS-B
+    V = FunctionSpace(mesh, ele)
+    q = Function(V)
+    c = Control(q)
+    J, u = run_model(q)
+    rf = ReducedFunctional(J, c)
+
+    qopt = minimize(rf, bounds=bounds, options={"disp": True, "gtol": 1e-10, "ftol": 1e-12,
+                                                'maxiter': 200})
+    # run model again for output with optimal control
+    J, u = run_model(qopt)
+else:
+    minimize_gs(run_model, mesh, ele, bounds=bounds,
+                params={'max_adapts': 100})
